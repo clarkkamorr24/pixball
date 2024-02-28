61bc013e587835b56f30dfd2348d2fc11179c264
clarkkamorr24 <biancake0803@gmail.com>
2024-02-28 15:28:36 +0800

    buttton size

--- src/components/Casinos/TableContent/Status.jsx
+++ src/components/Casinos/TableContent/Status.jsx
@@ -9,7 +9,7 @@ const Status = (props) => {
           className="flex cursor-pointer items-center"
         >
           <input type="checkbox" id={props.cell.id} className="peer sr-only" />
-          <div className="relative block  h-6 w-12 rounded-full bg-red p-1 text-xs before:absolute before:left-1 before:h-4 before:w-4 before:rounded-full before:bg-white before:p-1 before:transition-all before:duration-500 peer-checked:bg-success peer-checked:before:left-7 peer-checked:before:bg-white"></div>
+          <div className="relative block h-5 w-10 rounded-full bg-red p-1 text-xs before:absolute before:left-1 before:h-3 before:w-3 before:rounded-full before:bg-white before:p-1 before:transition-all before:duration-500 peer-checked:bg-success peer-checked:before:left-6 peer-checked:before:bg-white"></div>
         </label>
       </div>
     </div>
